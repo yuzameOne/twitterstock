8d3254690131af17d20a9d5bc7d9db11bb966d98
yuzameOne <E.Homayko@yandex.ru>
2020-04-08 04:15:16 +1000

    authorization completed

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
+const puppeteer = require('puppeteer');
 const negativeParse = require("./parse/negativeParse");
 const positiveParse = require("./parse/positiveParse");
 
 
-negativeParse().then(
-    (result)=>{
-        console.log(`tik: ${result.row1[0]}, open: ${result.row1[4]}, close: ${result.row1[2]}, per: ${result.row1[3]}% `)
-        console.log(`tik: ${result.row2[0]}, open: ${result.row2[4]}, close: ${result.row2[2]}, per: ${result.row2[3]}% `)
-        console.log(`tik: ${result.row3[0]}, open: ${result.row3[4]}, close: ${result.row3[2]}, per: ${result.row3[3]}% `)
-        console.log(`tik: ${result.row4[0]}, open: ${result.row4[4]}, close: ${result.row4[2]}, per: ${result.row4[3]}% `)
-        console.log(`tik: ${result.row5[0]}, open: ${result.row5[4]}, close: ${result.row5[2]}, per: ${result.row5[3]}% `)
-       
-    }
-)
+let pwd ='xxxxx'
 
-// positiveParse().then(
+let signIn = async () =>{
+
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1000,
+        height: 800,
+      });
+
+      await page.goto("https://twitter.com/home") 
+
+      await page.waitFor(2000)
+      await page.$('input[name="session[username_or_email]"]') 
+      await page.keyboard.type('MoexStocks',{delay: 300}); 
+      await page.$('input[name="session[password]"]')
+      await page.focus('input[name="session[password]"]')
+      await page.keyboard.type(pwd,{delay: 300});
+      let log = await page.$('div[data-testid="LoginForm_Login_Button"]')
+      log.click()
+
+      const inputTweet = await page.$('span[data-offset-key]');
+
+    }
+signIn();
+
+
+// negativeParse().then(
 //     (result)=>{
-//         console.log(`tikets : ${result.row1[0]}, name : ${result.row1[1]}, percent : +${result.row1[3]}%`)
-//         console.log(`tikets : ${result.row2[0]}, name : ${result.row2[1]}, percent : +${result.row2[3]}%`)
-//         console.log(`tikets : ${result.row3[0]}, name : ${result.row3[1]}, percent : +${result.row3[3]}%`)
-//         console.log(`tikets : ${result.row4[0]}, name : ${result.row4[1]}, percent : +${result.row4[3]}%`)
-//         console.log(`tikets : ${result.row5[0]}, name : ${result.row5[1]}, percent : +${result.row5[3]}%`)
+//         console.log(`tik: ${result.row1[0]}, open: ${result.row1[4]}, close: ${result.row1[2]}, per: ${result.row1[3]}% `)
+//         console.log(`tik: ${result.row2[0]}, open: ${result.row2[4]}, close: ${result.row2[2]}, per: ${result.row2[3]}% `)
+//         console.log(`tik: ${result.row3[0]}, open: ${result.row3[4]}, close: ${result.row3[2]}, per: ${result.row3[3]}% `)
+//         console.log(`tik: ${result.row4[0]}, open: ${result.row4[4]}, close: ${result.row4[2]}, per: ${result.row4[3]}% `)
+//         console.log(`tik: ${result.row5[0]}, open: ${result.row5[4]}, close: ${result.row5[2]}, per: ${result.row5[3]}% `)
+       
 //     }
 // )
 
-// 014268
+ 
+
+
+// positiveParse().then(
+//     (result)=>{
+//         console.log(`tik: ${result.row1[0]}, open: ${result.row1[4]}, close: ${result.row1[2]}, per: +${result.row1[3]}% `)
+//         console.log(`tik: ${result.row2[0]}, open: ${result.row2[4]}, close: ${result.row2[2]}, per: +${result.row2[3]}% `)
+//         console.log(`tik: ${result.row3[0]}, open: ${result.row3[4]}, close: ${result.row3[2]}, per: +${result.row3[3]}% `)
+//         console.log(`tik: ${result.row4[0]}, open: ${result.row4[4]}, close: ${result.row4[2]}, per: +${result.row4[3]}% `)
+//         console.log(`tik: ${result.row5[0]}, open: ${result.row5[4]}, close: ${result.row5[2]}, per: +${result.row5[3]}% `)
+//     }
+// )
